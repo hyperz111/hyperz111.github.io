2933003e6e74063b4c8d25d5c8811edd77a34836
hyperz111 <firzeroun11@gmail.com>
2025-08-10 16:51:42 +0700

    some edit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,3 @@
-export default {
-	semi: true,
-	singleQuote: true,
-	tabWidth: 2,
-	printWidth: 125,
-	useTabs: true,
-	bracketSpacing: true,
-	arrowParens: "always"
-}
+import config from "@hyperz111/prettier-config";
+
+export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 /** @type {import('vite').UserConfig} */
-import { defineConfig } from 'vite';
-import prismjs from 'vite-plugin-prismjs'
+import { defineConfig } from "vite";
+import prismjs from "vite-plugin-prismjs";
 
 export default defineConfig({
-  plugins: [
-    prismjs({
-      languages: ['javascript'],
-      plugins: ['normalize-whitespace'],
-      theme: 'tomorrow',
-      css: true
-    }),
-  ],
+	plugins: [
+		prismjs({
+			languages: ["javascript"],
+			plugins: ["normalize-whitespace"],
+			theme: "tomorrow",
+			css: true,
+		}),
+	],
 });
