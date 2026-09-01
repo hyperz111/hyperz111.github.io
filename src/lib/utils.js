e20815f443468416065a5c975871f30f7de9328f
hyperz111 <firzeroun11@gmail.com>
2026-09-01 19:54:11 +0700

    chore: add jsdoc

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,15 @@
+/**
+ * @param {string} path
+ * @returns {string | null}
+ */
 export function slugFromPath(path) {
 	return path.match(/([\w-]+)\.(md)/i)?.[1] ?? null;
 }
 
+/**
+ * @param {string} date
+ * @returns {string}
+ */
 export function formatDate(date) {
 	date = new Date(date);
 	return date.toLocaleDateString("en-US", {
